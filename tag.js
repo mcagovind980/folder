315739ae6f735330d66ed36fe2f500bdac04ee42
mcagovind980 <kg878672@gmail.com>
2026-06-05 16:00:56 +0530

    m

---
+++ tag.js
@@ -0,0 +1,19 @@
+// const element=document.getElementById("in");
+// document.getElementById("demo").innerHTML="The  Text from the intro " +element[0].innerHTML;
+
+// const element = document.getElementsByTagName("p");
+
+// document.getElementById("demo").innerHTML = 'The text is:' + element[2].innerHTML;
+
+// document.getElementById("demo").innerHTML="the text"+element[3].innerHTML;
+
+// document.getElementById("demo").innerHTML="<h1>  Text1  "+ element[3].innerHTML
+
+// const x=document.getElementById("p");
+// const y=x.getElementsByTagName("p");
+// document.getElementById("demo").innerHTML="the first paragraph inside main is"+y[0].innerHTML;
+
+
+// const my=document.querySelector(".demo");
+// my.innerHTML="Hello World"
+
